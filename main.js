d584c8e866920cbd9b0dfadcb6a0aa37bd8cd3b0
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2023-07-25 22:43:10 +0630

    character vs update

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 const characterNames = ['Albedo','Alhaitham','Aloy','Amber','Itto','Baizhu','Barbara','Beidou','Bennett','Candace','Chongyun','Collei','Cyno','Dehya','Diluc','Diona','Dori','Eula','Faruzan','Fischl','Ganyu','Gorou','Hutao','Jean','Kazuha','Kaeya','Ayaka','Ayato','Kaveh','Keqing','Kiara','Klee','K-Sara','Shinobu','Layla','Lisa','Mika','Mona','Nahida','Nilou','Ningguang','Noelle','Qiqi','Raiden','Razor','Rosaria','Kokomi','Sayu','Shenhe','Heizou','Sucrose','Childe','Thoma','Tighnari','Aether','Lumine','Venti','Scara','Xianglin','Xiao','Xingqiu','Xinyan','YaeMiko','Yanfei','Yaoyao','Yelan','Yoimiya','YunJin','Zhongli']
 const characterWeapon = ['Broom','Slipper','Belt','Knife','Grenade','Stick','Ak47','Dynamite','Sword','Spear','WaterBucket','HandGun']
+const locations = ['Monstaldt','Liyue','Inuzuma','Sumeru','Fountain']
+const charMindState = ['Happy','Sad','Powerful','Playful','Lucky','Dead','Hungry','Unlucky','Horny','Feard']
 
 let characterOnField = {}
 
 function addCharacters() {
-    let name =  characterNames[Math.round(Math.random()*characterNames.length-1)]
-    
-    // console.log(characterOnField.hasOwnProperty(name))
-
+    console.log(Object.keys(characterOnField).length+ " - "+characterNames.length)
+    if(Object.keys(characterOnField).length == characterNames.length){
+        alert('Maximum characters reached')
+    }else{
+        let name =  characterNames[Math.round(Math.random()*(characterNames.length-1))]
+    let mind = charMindState[Math.floor(Math.random()*(charMindState.length-1))]
     while(characterOnField.hasOwnProperty(name)){
-        name =  characterNames[Math.round(Math.random()*characterNames.length-1)]
+        name =  characterNames[Math.round(Math.random()*(characterNames.length-1))]
+    }
+    let health = 100
+    //Check Mind condition
+    if (mind == 'Dead') {
+        health = 1
+    }else if(mind == 'Horny'){
+        health = 69
+    }else if(mind == 'Unlucky'){
+        health = Math.round(Math.random() * 50) 
     }
 
-    let health = Math.round(Math.random() * 100)
     let startingweapon = characterWeapon[Math.round(Math.random()*characterWeapon.length)]
     const characterData = {
         charName : name,
@@ -21,9 +33,12 @@ function addCharacters() {
         skill1 : true,
         skill2: false,
         passive: true,
+        currentLocation: locations[Math.floor(Math.random()*(locations.length-1))],
+        mindState: mind,
+        kill: 0,
+        run: 0,
     }
     characterOnField[name] = characterData
-    console.log(characterData)
 
     var newCharacter = document.createElement('div')
     var parent = document.querySelector('.charactersBox')
@@ -49,6 +64,7 @@ function addCharacters() {
 
     // update Data
     updateCounts()
+    }
 }
 
 window.addEventListener("load", (event) => {
@@ -69,16 +85,13 @@ function updateCounts(){
         }else{
             dc++
         }
-        console.log(`${key}: ${characterOnField[key].charHealth}`);
     })
-    console.log(lc)
     document.querySelector('.aliveCount').innerHTML = lc
     document.querySelector('.deadCount').innerHTML = dc
 }
 
 function scrollToElement(dir){
     const scrollTo = document.querySelectorAll('.characterBox')
-    console.log(scrollTo)
     if(dir == 'up'){
         scrollTo[0].scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" })
     }else{
